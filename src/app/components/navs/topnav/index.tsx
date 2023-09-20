78b19e9b4467108648238918c65caf8c040d6723
dayo4 <dayo4@live.com>
2023-09-20 19:50:24 +0000

    Sep 20, 2023, 8:50 PM

--- src/app/components/navs/topnav/index.tsx
+++ src/app/components/navs/topnav/index.tsx
@@ -1,14 +1,25 @@
+import Icon from '@mdi/react';
+import {
+   mdiMenu
+} from '@mdi/js';
+import Logo from '@/assets/icons/logo.svg';
+import Image from 'next/image'
 
 export const TopNav = ({
-    // children,
- }: TopNavProps) => {
-    return (
-       <div className="h-[65px]">
-         
-       </div>
-    )
- }
- 
- interface TopNavProps {
-    // children: React.ReactNode
- }
+   // children,
+}: TopNavProps) => {
+   return (
+      <div className="flex justify-between align-middle h-[65px] border-b-2 border-my-pri-color">
+         {/* <div className=''> */}
+            <Image src={Logo} alt='Reni store logo' width={162} height={31.05}></Image>
+         {/* </div> */}
+         <Icon className=""
+            path={mdiMenu}
+            size={1.1} />
+      </div>
+   )
+}
+
+interface TopNavProps {
+   // children: React.ReactNode
+}
