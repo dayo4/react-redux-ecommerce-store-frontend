8dd4931e93a6d3cd0e7a30bed901bf17e82c1760
dayo4 <dayo4@live.com>
2023-09-20 15:28:04 +0000

    Sep 20, 2023, 4:28 PM

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,13 +1,17 @@
-import {FadeLoader} from "react-spinners";
+import { FadeLoader } from "react-spinners";
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    return <FadeLoader
-        // color={color}
-        loading={true}
-        // cssOverride={override}
-        // size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-    />
+    return (
+        <div className="flex justify-center align-middle bg-neutral-500">
+            <FadeLoader
+                color={'#3690FF'}
+                loading={true}
+                // cssOverride={override}
+                // size={150}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        </div>
+    )
 }
