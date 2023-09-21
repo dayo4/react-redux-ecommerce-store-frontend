c7a79581716a2e3a9ebca48c955e92e4df672ff0
dayo4 <dayo4@live.com>
2023-09-21 08:49:38 +0000

    Sep 21, 2023, 9:49 AM

--- src/app/components/navs/topnav/index.tsx
+++ src/app/components/navs/topnav/index.tsx
@@ -12,6 +12,10 @@ export const TopNav = ({
       <div className="flex justify-between align-middle h-[65px] border-b-2 border-my-pri-color">
          {/* <div className=''> */}
             <Image src={Logo} alt='Reni store logo' width={162} height={31.05}></Image>
+            <div className="mb-3">
+                {/* <label  className="text-sm text-navy-700 dark:text-white font-bold">Default</label> */}
+                <input type="search" placeholder="search" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-gray-200"/>
+            </div>
          {/* </div> */}
          <Icon className=""
             path={mdiMenu}
