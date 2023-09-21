dd4fce3257c580c0302573014a5df038a02b98f1
dayo4 <dayo4@live.com>
2023-09-21 10:31:33 +0100

    coded topnav

--- src/app/components/navs/topnav/index.tsx
+++ src/app/components/navs/topnav/index.tsx
@@ -3,6 +3,8 @@ import {
    mdiMenu
 } from '@mdi/js';
 import Logo from '@/assets/icons/logo.svg';
+import PhoneIcon from '@/assets/icons/phone.svg';
+import FunnelMenuIcon from '@/assets/icons/funnel-menu.svg';
 import Image from 'next/image'
 
 export const TopNav = ({
@@ -10,16 +12,18 @@ export const TopNav = ({
 }: TopNavProps) => {
    return (
       <div className="flex justify-between align-middle h-[65px] border-b-2 border-my-pri-color">
-         {/* <div className=''> */}
-            <Image src={Logo} alt='Reni store logo' width={162} height={31.05}></Image>
-            <div className="">
-                {/* <label  className="text-sm text-navy-700 dark:text-white font-bold">Default</label> */}
-                <input type="search" placeholder="search" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-lg outline-none border-gray-700"/>
-            </div>
-         {/* </div> */}
-         <Icon className=""
+         <Image src={Logo} alt='Store logo' width={162} height={31.05} className="ml-5"></Image>
+         <div className="">
+            {/* <label  className="text-sm text-navy-700 dark:text-white font-bold">Default</label> */}
+            <input type="search" placeholder="Search" className="mt-2 flex h-11 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-lg outline-none border-gray-400" />
+         </div>
+         <div className='flex mx-4'>
+            <Image src={PhoneIcon} alt="Phone Icon" className='mr-5 cursor-pointer' height={22}></Image>
+            <Image src={FunnelMenuIcon} alt="Menu Icon" className='mr-5 cursor-pointer' height={30}></Image>
+         </div>
+         {/* <Icon className=""
             path={mdiMenu}
-            size={1.1} />
+            size={1.1} /> */}
       </div>
    )
 }
