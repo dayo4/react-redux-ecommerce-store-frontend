0b291d5440d1ff679f41da569116f58103491eea
dayo4 <dayo4@live.com>
2023-09-24 16:08:17 +0100

    edited 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link'
- 
+import {
+  Button,
+} from "@/app/components/materials";
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className='w-full h-full flex justify-center align-middle text-center'>
+      <div className='basis-11/12 sm:basis-10/12 lg:basis-8/12'>
+        <h1 className='text-[60px]'>404 Not Found</h1>
+        <p>Could not find the page you are looking for</p>
+
+        <Button variant="filled" className="bg-my-pri-color mb-2 my-10 rounded-md">
+          <Link href={"/"} className={" flex pr-4 font-bold"} >
+            Back to Home Page
+          </Link>
+        </Button>
+      </div>
     </div>
   )
 }
