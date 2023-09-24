abfaa5fd7ac3fba63e0844d7efe04e41d8be8f45
dayo4 <dayo4@live.com>
2023-09-24 17:54:48 +0100

    added profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,117 @@
+import Icon from '@mdi/react';
+import {
+    mdiHeart, mdiHeartOutline, mdiMagnify, mdiStoreSearch, mdiTrashCan, mdiTrashCanOutline
+} from '@mdi/js';
+import Image from 'next/image'
+import Link from 'next/link'
+import { } from '@/redux'
+import { PriSectionHeader } from '@/app/components/headers'
+import {
+    Rating,
+    Input,
+    Spinner,
+    Card,
+    CardHeader,
+    Typography,
+    Button,
+    CardBody,
+    Chip,
+    CardFooter,
+    Avatar,
+    IconButton,
+    Tooltip,
+} from "@/app/components/materials";
+
+
+
+export default function Reviews() {
+
+    const TABLE_HEAD = ["Product", "Price", "Quantity", "Action", "Subtotal"];
+
+    const TABLE_ROWS = [
+        {
+            img: "/img/item1.png",
+            name: "Custom Gown",
+            price: "$2,500",
+            quantity: "2",
+            action: "Delete",
+            subtotal: "$5,000",
+        },
+        {
+            img: "/img/item1.png",
+            name: "Custom Gown",
+            price: "$2,500",
+            quantity: "2",
+            action: "Delete",
+            subtotal: "$5,000",
+        },
+        {
+            img: "/img/item1.png",
+            name: "Custom Gown",
+            price: "$2,500",
+            quantity: "2",
+            action: "Delete",
+            subtotal: "$5,000",
+        },
+    ];
+
+    return (
+        <div>
+
+            <div className='flex justify-center'>
+                <div className='p-3 basis-11/12 md:basis-10/12'>
+                    <section className='flex justify-between my-4'>
+                        <span></span>
+                        <span className='font-[500]'>Welcome! <span className='text-my-pri-color'>Olayinka</span></span>
+                    </section>
+
+                    <section className=''>
+                        <div className='flex flex-wrap justify-between'>
+                            <div className='basis-full sm:basis-10/12 lg:basis-5/12 mb-8'>
+                                <h3 className='font-bold mb-2'>First Name</h3>
+                                <Input variant='standard' type='text' className='w-full bg-[#F5F5F5] pl-2' />
+                            </div>
+                            <div className='basis-full sm:basis-10/12 lg:basis-5/12 mb-8'>
+                                <h3 className='font-bold mb-2'>First Name</h3>
+                                <Input variant='standard' type='text' className='w-full bg-[#F5F5F5] pl-2' />
+                            </div>
+                            <div className='basis-full sm:basis-10/12 lg:basis-5/12 mb-8'>
+                                <h3 className='font-bold mb-2'>First Name</h3>
+                                <Input variant='standard' type='text' className='w-full bg-[#F5F5F5] pl-2' />
+                            </div>
+                            <div className='basis-full sm:basis-10/12 lg:basis-5/12 mb-8'>
+                                <h3 className='font-bold mb-2'>First Name</h3>
+                                <Input variant='standard' type='text' className='w-full bg-[#F5F5F5] pl-2' />
+                            </div>
+                        </div>
+
+                        <hr className='my-8' />
+
+                        <div className='flex flex-wrap justify-between'>
+                            <h3 className='basis-full font-bold mb-4'>Password Changes</h3>
+                            <div className='basis-full sm:basis-10/12 lg:basis-5/12 mb-8'>
+                                <Input variant='standard' label='Old Password' autoComplete='false' type='password' className='w-full bg-[#F5F5F5] pl-2' />
+                            </div>
+                            <div className='basis-full sm:basis-10/12 lg:basis-5/12 mb-8'>
+                                <Input variant='standard' label='New Password' autoComplete='false' type='password' className='w-full bg-[#F5F5F5] pl-2' />
+                            </div>
+                        </div>
+
+                        <div className='flex justify-between'>
+                            <div></div>
+                            <div className='mb-6'>
+                                <Button variant="text" className="mb-2 rounded-md mr-3">
+                                    Cancel
+                                </Button>
+                                <Button variant="filled" className="mb-2 bg-my-pri-color rounded-md">
+                                    Save Changes
+                                </Button>
+                            </div>
+                        </div>
+
+                    </section>
+                </div>
+            </div>
+        </div>
+    )
+}
