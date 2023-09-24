fe9bd335f3dadd9b68e79a2b1754fe86d7b6086d
dayo4 <dayo4@live.com>
2023-09-24 16:06:53 +0100

    edited spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,17 +1,19 @@
-import { FadeLoader } from "react-spinners";
+// import { FadeLoader } from "react-spinners";
+import { Spinner } from "@/app/components/materials";
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return (
         <div className="flex justify-center align-middle w-full h-full bg-neutral-300">
-            <FadeLoader
+            {/* <FadeLoader
                 color={'#3690FF'}
                 loading={true}
                 // cssOverride={override}
                 // size={150}
                 aria-label="Loading Spinner"
                 data-testid="loader"
-            />
+            /> */}
+                  <Spinner  color="green" className="h-14 w-14" />
         </div>
     )
 }
