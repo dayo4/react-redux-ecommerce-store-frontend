c6aa3cc8e1d007578e393a45e87106f1633244e7
dayo4 <dayo4@live.com>
2023-09-25 13:30:23 +0100

    updated

--- src/app/components/headers/top-breadcrumbs/index.tsx
+++ src/app/components/headers/top-breadcrumbs/index.tsx
@@ -1,19 +1,53 @@
 import { Url } from 'next/dist/shared/lib/router/router'
 import Link from 'next/link'
+import Icon from '@mdi/react';
+import {
+    mdiAlert,
+    mdiAlertBox,
+    mdiArrowRight,
+    mdiHeart, mdiHeartOutline, mdiHome, mdiMagnify, mdiStoreSearch, mdiTrashCan, mdiTrashCanOutline
+} from '@mdi/js';
+import {
+    Rating,
+    Input,
+    Spinner,
+    Card,
+    CardHeader,
+    Typography,
+    Button,
+    CardBody,
+    Chip,
+    CardFooter,
+    Avatar,
+    IconButton,
+    Tooltip,
+    Checkbox,
+    Alert,
+    Breadcrumbs
+} from "@/app/components/materials";
 
 export const TopBreadcrumb = ({
     links
 }: TopBreadcrumbProps) => {
     return (
-        <div>
+        <Breadcrumbs
+            separator={
+                <Icon path={mdiArrowRight} size={1} />
+            }
+            className="rounded-full border border-white bg-gradient-to-tr from-gray-900 to-gray-800 p-1 mt-5 mb-2"
+        >
+            <Link href={"/"} className="rounded-full bg-white px-3 py-1 font-medium text-gray-900">
+                {<Icon path={mdiHome} size={1} />}
+            </Link>
             {links.map((link, i) => {
                 return (
-                    <Link href={link.href} key={i}>
-                        {link.title + (i < links.length ? ' / ' : '')}
+                    <Link href={link.href} className="rounded-full bg-white px-3 py-1 font-medium text-gray-900" key={i}>
+                        {link.title}
                     </Link>
                 )
             })}
-        </div>
+
+        </Breadcrumbs>
     )
 }
 
