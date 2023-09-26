4548523b26477f6a6cddfbec7e11ec0188f6fba6
dayo4 <dayo4@live.com>
2023-09-26 13:11:52 +0100

    added items carousel

---
+++ src/app/components/utils/items-carousel/index.tsx
@@ -0,0 +1,18 @@
+
+export const ItemsCarousel = ({
+    className,
+    children
+}: PriSectionHeaderProps) => {
+    return (
+        <div className="overflow-y-hidden w-full">
+            <div className={className + ' ' + 'flex align-middle overflow-x-scroll translate-y-5'}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+interface PriSectionHeaderProps {
+    className?: string
+    children: React.ReactNode
+}
