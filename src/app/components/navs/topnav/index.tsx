4e643287c8b8e6b4d3f674e00cd24ab30827d0f1
dayo4 <dayo4@live.com>
2023-09-26 19:03:55 +0100

    updated

--- src/app/components/navs/topnav/index.tsx
+++ src/app/components/navs/topnav/index.tsx
@@ -19,26 +19,30 @@ export const TopNav = ({
 }: TopNavProps) => {
    return (
       <div className="flex justify-between align-middle h-[65px] absolute top-0 left-0 z-[200] bg-white w-full border-b-2 border-my-pri-color">
-         <Image src={Logo} alt='Store logo' width={162} height={31.05} className="ml-12"></Image>
+         <div className='flex justify-center align-middle w-[90px] lg:w-[132px] lg:h-[31px] lg:mt-3'>
+            <img src={"/icons/logo.svg"} alt='Store logo' /* width={162} height={31.05} */ className="max-w-full max-h-full ml-8 sm:ml-12" />
+         </div>
          <div className="hidden md:flex align-middle mt-3">
-            {/* <label  className="text-sm text-navy-700 dark:text-white font-bold">Default</label> */}
             {/* <input type="search" placeholder="Search here" className="mt-2 flex h-11 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-lg outline-none border-gray-300" /> */}
             <Input
                label="Search"
                type={'search'}
-               // className='mt-6'
-               icon={<Icon path={mdiMagnify} className='cursor-pointer' size={1.1} />}
+               icon={
+               // <Button variant='text' className='p- text-gray-600'>
+                  <Icon path={mdiMagnify} className='cursor-pointer p-1 rounded-md hover:bg-gray-400 active:bg-gray-500 transition-colors' size={1.1} />
+               // </Button>
+               }
             />
          </div>
-         <div className='flex align-middle mx-4'>
-            <IconButton variant='text' className='mr-6'>
-               <Image src={PhoneIcon} alt="Phone Icon" height={22}></Image>
+         <div className='flex align-middle mx-4 mt-3'>
+            <IconButton variant='text' className='mr-2 p-0'>
+               <Image src={PhoneIcon} alt="Phone Icon" height={22} width={22}></Image>
             </IconButton>
-            <IconButton variant='text' className='mr-6'>
-               <Image src={NavCartIcon} alt="NavCart Icon" height={26}></Image>
+            <IconButton variant='text' className='mr-2'>
+               <Image src={NavCartIcon} alt="NavCart Icon" height={26} width={26}></Image>
             </IconButton>
-            <IconButton variant='text' className='mr-6'>
-               <Image src={FunnelMenuIcon} alt="Menu Icon" height={30}></Image>
+            <IconButton variant='text' className='mr-0 md:mr-6'>
+               <Image src={FunnelMenuIcon} alt="Menu Icon" height={30} width={30}></Image>
             </IconButton>
          </div>
       </div>
