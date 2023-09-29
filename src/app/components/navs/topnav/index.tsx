59e696462e8ef2a4f617c993cc530471e9b320cc
dayo4 <dayo4@live.com>
2023-09-29 15:14:20 +0100

    updated

--- src/app/components/navs/topnav/index.tsx
+++ src/app/components/navs/topnav/index.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Logo from '@/assets/icons/logo.svg';
 import PhoneIcon from '@/assets/icons/phone.svg';
 import NavCartIcon from '@/assets/icons/nav-cart.svg';
 import FunnelMenuIcon from '@/assets/icons/funnel-menu.svg';
+import { useDispatch, useSelector, NavSlice } from '@/redux'
 import Image from 'next/image'
+import Link from 'next/link'
 import Icon from '@mdi/react';
 import {
+   mdiAccountPlusOutline,
+   mdiLoginVariant,
    mdiMagnify
 } from '@mdi/js';
 import {
@@ -23,7 +28,18 @@ import {
 export const TopNav = ({
    // children,
 }: TopNavProps) => {
+   const dispatch = useDispatch()
+   const nav = useSelector((state) => state.nav)
+   const { hideSideNAv, showSideNAv } = NavSlice
 
+   function handleSideNav() {
+      if (nav.status && nav.isSmallScreen) {
+         dispatch(hideSideNAv())
+      }
+      else {
+         dispatch(showSideNAv())
+      }
+   }
 
    const UserMenu = () => {
       return (
@@ -53,9 +69,11 @@ export const TopNav = ({
                         d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
                      />
                   </svg>
-                  <Typography variant="small" className="font-normal">
-                     My Profile
-                  </Typography>
+                  <Link href={'/profile'}>
+                     <Typography variant="small" className="font-normal">
+                        My Profile
+                     </Typography>
+                  </Link>
                </MenuItem>
                <MenuItem className="flex items-center gap-2">
                   <svg
@@ -78,26 +96,7 @@ export const TopNav = ({
                      />
                   </svg>
                   <Typography variant="small" className="font-normal">
-                     Edit Profile
-                  </Typography>
-               </MenuItem>
-               <MenuItem className="flex items-center gap-2">
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     viewBox="0 0 24 24"
-                     strokeWidth={2}
-                     stroke="currentColor"
-                     className="h-4 w-4"
-                  >
-                     <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"
-                     />
-                  </svg>
-                  <Typography variant="small" className="font-normal">
-                     Inbox
+                     Settings
                   </Typography>
                </MenuItem>
                <MenuItem className="flex items-center gap-2">
@@ -121,23 +120,20 @@ export const TopNav = ({
                </MenuItem>
                <hr className="my-2 border-blue-gray-50" />
                <MenuItem className="flex items-center gap-2 ">
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     viewBox="0 0 24 24"
-                     strokeWidth={2}
-                     stroke="currentColor"
-                     className="h-4 w-4"
-                  >
-                     <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"
-                     />
-                  </svg>
-                  <Typography variant="small" className="font-normal">
-                     Sign Out
-                  </Typography>
+                  <Icon path={mdiLoginVariant} size={0.7} />
+                  <Link href={'/login'}>
+                     <Typography variant="small" className="font-normal">
+                        Sign In
+                     </Typography>
+                  </Link>
+               </MenuItem>
+               <MenuItem className="flex items-center gap-2 ">
+                  <Icon path={mdiAccountPlusOutline} size={0.7} />
+                  <Link href={'/register'}>
+                     <Typography variant="small" className="font-normal">
+                        Create Account
+                     </Typography>
+                  </Link>
                </MenuItem>
             </MenuList>
          </Menu>
@@ -162,16 +158,20 @@ export const TopNav = ({
             />
          </div>
          <div className='flex align-middle mx-4 mt-3'>
-         <UserMenu></UserMenu>
+            <UserMenu></UserMenu>
             {/* <IconButton variant='text' className='mr-2 p-0'>
                <Image src={PhoneIcon} alt="Phone Icon" height={22} width={22}></Image>
             </IconButton> */}
+            <div className='md:mr-4 lg:mr-6'>
             <Badge color='pink' className='top-3 right-4 w-2'>
                <IconButton variant='text' className='mr-2'>
-                  <Image src={NavCartIcon} alt="NavCart Icon" height={26} width={26}></Image>
+                  <Link href={'/cart'}>
+                     <Image src={NavCartIcon} alt="NavCart Icon" height={26} width={26}></Image>
+                  </Link>
                </IconButton>
             </Badge>
-            <IconButton variant='text' className='mr-0 md:mr-6'>
+            </div>
+            <IconButton onClick={() => handleSideNav()} variant='text' className='mr-0 md:mr-6 lg:hidden'>
                <Image src={FunnelMenuIcon} alt="Menu Icon" height={30} width={30}></Image>
             </IconButton>
          </div>
