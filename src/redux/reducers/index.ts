2b59a3507a95102ebdc9254f27bcc6e520f5412b
dayo4 <dayo4@live.com>
2023-09-29 15:15:29 +0100

    updated

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,10 @@
 /* Instruments */
 import { userApi } from "../queries/userApi";
 import counterReducer  from '../slices/counterSlice'
+import navReducer  from '../slices/navSlice'
 
 export const reducer = {
   counter: counterReducer,
+  nav: navReducer,
   [userApi.reducerPath]: userApi.reducer,
 }
