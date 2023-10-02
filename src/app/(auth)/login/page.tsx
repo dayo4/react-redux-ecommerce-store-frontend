e43ba8fe6d51bef3543ab4ddd5e2848e8912c025
dayo4 <dayo4@live.com>
2023-10-02 17:14:30 +0100

    updated

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,17 @@
+'use client'
+import {
+    Input,
+    Button,
+    Avatar,
+    IconButton,
+    Badge,
+    Menu,
+    MenuHandler,
+    MenuList,
+    MenuItem,
+    Typography
+ } from "@material-tailwind/react";
+
 
 
 export default function Login(props: React.PropsWithChildren) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { useDispatch, useSelector, CounterSlice } from '@/redux'
 import { useGetUserByIdQuery } from '@/redux/queries'
 import { PriSectionHeader, ItemsCarousel } from './components/utils'
-import { Button, Rating } from "@/app/components/materials";
+import { Button, Rating } from "@material-tailwind/react";
 import { user } from "@/redux/dummy";
 
 export async function getServerSideProps() {
@@ -28,15 +28,15 @@ export default function Home() {
   // const { data: user } = useGetUserByIdQuery({ id: "" })
 
   const Items = [
-    { name: "Printed Gown", link: "/item", img: "/img/item1.png", price: "$39.00", rating: 4 },
-    { name: "Printed Gown", link: "/item", img: "/img/item2.png", price: "$39.00", rating: 2 },
-    { name: "Printed Gown", link: "/item", img: "/img/item3.png", price: "$39.00", rating: 4 },
-    { name: "Printed Gown", link: "/item", img: "/img/item1.png", price: "$39.00", rating: 3 },
-    { name: "Printed Gown", link: "/item", img: "/img/item2.png", price: "$39.00", rating: 4 },
-    { name: "Printed Gown", link: "/item", img: "/img/item3.png", price: "$39.00", rating: 4 },
-    { name: "Printed Gown", link: "/item", img: "/img/item1.png", price: "$39.00", rating: 3 },
-    { name: "Printed Gown", link: "/item", img: "/img/item2.png", price: "$39.00", rating: 3 },
-    { name: "Printed Gown", link: "/item", img: "/img/item3.png", price: "$39.00", rating: 4 },
+    { name: "Printed Gown", link: "/item", img: "/img/item1.png", price: "$39.00", rating: 4.9 },
+    { name: "Printed Gown", link: "/item", img: "/img/item2.png", price: "$39.00", rating: 2.4 },
+    { name: "Printed Gown", link: "/item", img: "/img/item3.png", price: "$39.00", rating: 4.4 },
+    { name: "Printed Gown", link: "/item", img: "/img/item1.png", price: "$39.00", rating: 3.3 },
+    { name: "Printed Gown", link: "/item", img: "/img/item2.png", price: "$39.00", rating: 4.5 },
+    { name: "Printed Gown", link: "/item", img: "/img/item3.png", price: "$39.00", rating: 4.1 },
+    { name: "Printed Gown", link: "/item", img: "/img/item1.png", price: "$39.00", rating: 3.8 },
+    { name: "Printed Gown", link: "/item", img: "/img/item2.png", price: "$39.00", rating: 3.0 },
+    { name: "Printed Gown", link: "/item", img: "/img/item3.png", price: "$39.00", rating: 4.6 },
   ]
 
   return (
@@ -59,18 +59,25 @@ export default function Home() {
       </section>
       <section>
 
-        
+
       </section>
       <section className="mt-14 pb-2">
         <PriSectionHeader title="New Release"></PriSectionHeader>
         <ItemsCarousel className='gap-x-4'>
           {Items.map((item, i) => {
             return (
-              <Link href={item.link} className='bg-[#FBFBFB] p-2 rounded-sm' key={i}>
-                <Image src={item.img} className="rounded-xl" alt={item.name} width={130} height={170}></Image>
-                <h3 className='text-[#7E7E7E] text-[12px] font-bold mt-2'>{item.name}</h3>
-                <p className='font-bold mt-1'> {item.price}</p>
-                <Rating readonly value={item.rating} className='my-1 -ml-1' />
+              <Link href={item.link} className='min-w-[150px] max-w-[200px] bg-[#FBFBFB] p-2 rounded-sm' key={i}>
+                <div className='flex justify-center h-[calc(100%-88px)]'>
+                  <Image src={item.img} placeholder='empty' className="rounded-xl" alt={item.name} width={130} height={170}></Image>
+                </div>
+                <div className='h-[80px]'>
+                  <h3 className='text-[#7E7E7E] text-[12px] font-bold mt-2 whitespace-nowrap overflow-hidden'>{item.name}</h3>
+                  <p className='font-bold mt-1'> {item.price}</p>
+                  <div className="flex items-center mt-1">
+                    <svg className="w-5 h-5 -ml-1 text-yellow-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <p className="ml-1 text-sm font-bold text-gray-500 dark:text-white">{item.rating}</p>
+                  </div>
+                </div>
               </Link>
             )
           })}
@@ -82,11 +89,17 @@ export default function Home() {
         <ItemsCarousel className='gap-x-4'>
           {Items.map((item, i) => {
             return (
-              <Link href={item.link} className='bg-[#FBFBFB] p-2 rounded-sm' key={i}>
-                <Image src={item.img} className="rounded-xl" alt={item.name} width={130} height={170}></Image>
-                <h3 className='text-[#7E7E7E] text-[12px] font-bold mt-2'>{item.name}</h3>
-                <p className='font-bold mt-1'> {item.price}</p>
-                <Rating readonly value={item.rating} className='my-1 -ml-1' />
+              <Link href={item.link} className='min-w-[150px] max-w-[200px] bg-[#FBFBFB] p-2 rounded-sm' key={i}>
+                <div className='flex justify-center h-[calc(100%-88px)]'>
+                  <Image src={item.img} placeholder='empty' className="rounded-xl" alt={item.name} width={130} height={170}></Image>
+                </div>
+                <div className='h-[80px]'>
+                  <h3 className='text-[#7E7E7E] text-[12px] font-bold mt-2 whitespace-nowrap overflow-hidden'>{item.name}</h3>
+                  <p className='font-bold mt-1'> {item.price}</p>
+                  <div className="flex items-center mt-1">
+                    <svg className="w-5 h-5 -ml-1 text-yellow-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <p className="ml-1 text-sm font-bold text-gray-500 dark:text-white">{item.rating}</p>                  </div>
+                </div>
               </Link>
             )
           })}
